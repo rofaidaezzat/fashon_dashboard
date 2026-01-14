d561bc1d52801bdef746373581ccc68bf11a3e56
rofaidaezzat <rofaidaezzat@gmail.com>
2026-01-14 13:35:11 +0200

    stret

--- src/Components/ProductModal/CreateModal.tsx
+++ src/Components/ProductModal/CreateModal.tsx
@@ -30,7 +30,7 @@ const CreateModal: React.FC<CreateModalProps> = ({ isOpen, onClose }) => {
         
         if (images && images.length > 0) {
              images.forEach((img) => {
-                formData.append('image', img);
+                formData.append('images[]', img);
             });
         }
 
